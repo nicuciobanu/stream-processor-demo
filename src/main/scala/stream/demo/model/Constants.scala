9e5f9e84c4c14eb405245a47548cc7ff7f5863e4
nicuciobanu <nciobanu@ext.worten.pt>
2025-02-28 15:10:03 +0200

    Use the TTL time to one hour

--- src/main/scala/stream/demo/model/Constants.scala
+++ src/main/scala/stream/demo/model/Constants.scala
@@ -5,7 +5,7 @@ import scala.concurrent.duration.DurationInt
 object Constants {
   // or move to config file
   val Buffer_Size           = 1000
-  val Redis_TTL_Seconds     = 60 // TTL in seconds
+  val Redis_TTL_Seconds     = 3600 // TTL in seconds
   val Charging_Source_Delay = 1.seconds
   val Host                  = "localhost"
   val Port                  = 6379
